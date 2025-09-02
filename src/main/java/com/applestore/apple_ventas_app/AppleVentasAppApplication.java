10df894ec21a54f820065d71cba8a5383fe91d3b
RodrigoSY <sihues3@gmail.com>
2025-09-02 15:13:13 -0500

    C2: primera vista pagina principal

--- src/main/java/com/applestore/apple_ventas_app/AppleVentasAppApplication.java
+++ src/main/java/com/applestore/apple_ventas_app/AppleVentasAppApplication.java
@@ -1,5 +1,7 @@
 package com.applestore.apple_ventas_app;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppleVentasAppApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(AppleVentasAppApplication.class, args);
+
+		abrirNavegador("http://localhost:" + "3000");
 	}
 
+	// Abre el navegador en la URL de la aplicación
+	private static void abrirNavegador(String url) {
+		String os = System.getProperty("os.name").toLowerCase();
+		try {
+			if (os.contains("win")) {
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} else if (os.contains("mac")) {
+				Runtime.getRuntime().exec("open " + url);
+			} else if (os.contains("nix") || os.contains("nux")) {
+				Runtime.getRuntime().exec("xdg-open " + url);
+			} else {
+				System.out.println("No se pudo detectar el sistema operativo para abrir el navegador.");
+			}
+		} catch (IOException e) {
+			System.out.println("Error al intentar abrir el navegador: " + e.getMessage());
+		}
+	}
 }
